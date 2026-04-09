5d9b1ffa48c758244d5340848cc0b81e344fa41a
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-04-09 10:19:29 +0700

    first commit

---
+++ src/main/java/ra/edu/Session7Application.java
@@ -0,0 +1,13 @@
+package ra.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session7Application.class, args);
+    }
+
+}
---
+++ src/main/java/ra/edu/mapper/CandidateMapper.java
@@ -0,0 +1,4 @@
+package ra.edu.mapper;
+
+public class CandidateMapper {
+}
